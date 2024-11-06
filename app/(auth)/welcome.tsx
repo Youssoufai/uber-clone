01e5561b9cc688bbcec2ef351a9062857be15f94
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-11-06 19:45:59 +0100

    Onboarding

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,9 +1,15 @@
+import { router } from "expo-router";
 import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 const Onboarding = () => {
     return (
         <SafeAreaView className="flex h-full items-center justify-between bg-white">
-            <TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => {
+                    router.replace("/(auth)/sign-up");
+                }}
+                className="w-full flex justify-end items-end p-5"
+            >
                 <Text>Skip</Text>
             </TouchableOpacity>
         </SafeAreaView>
